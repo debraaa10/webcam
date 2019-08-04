cd851236eeb5a789ae3fbd530d08c3c13b98eac1
debraaa10 <debraaa010@gmail.com>
2019-08-04 13:32:35 +0700

    Create email.php

---
+++ email.php
@@ -0,0 +1,10 @@
+<?php
+
+/* CREATED BY IWAN STER */
+
+/* SUNGGUH TEGA KAU... MEMBERI IMBALAN HANYA 10K GOBLOK KAU */
+
+$subjek = 'Akun Mr. Fap - Fap';
+$mailto = 'rinarahmaw51@gmail.com'; //masukin email lo disini
+
+?>
